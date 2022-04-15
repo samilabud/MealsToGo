73dd250cdb61bfef79e1939841cd4442757707ed
sabud <sabud@pinterest.com>
2022-04-14 22:24:49 -0400

    Section 7, chapter 126, Creating A Typography Component and Cleanup

---
+++ src/features/restaurants/screens/restaurants-screen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import styled from 'styled-components/native';
+
+const RestaurantContainerSafe = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+const RestaurantSearchView = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantsScreen = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = (query) => setSearchQuery(query);
+  return (
+    <RestaurantContainerSafe>
+      <RestaurantSearchView>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </RestaurantSearchView>
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+    </RestaurantContainerSafe>
+  );
+};
